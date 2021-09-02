1598c49dcb21d2e0249dce1ef1dd4e89802c813c
GGunta <msdoors@gmail.com>
2021-09-02 10:07:32 +0300

    Initial Commit

---
+++ DemoSwiftUIConcept/DemoSwiftUIConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftUIConceptApp.swift
+//  DemoSwiftUIConcept
+//
+//  Created by gunta.golde on 02/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftUIConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
